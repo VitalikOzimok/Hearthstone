859485d46b7cda3b1c9c2f816e4c1b2323d37024
VitalikOzimok <pilteev111@gmail.com>
2025-04-13 17:14:09 +0300

    chore: незначительные изменения

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    // Позволяет Jest игнорировать импорты CSS и алиасы
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "^@/(.*)$": "<rootDir>/src/$1", // если ты используешь алиас @/ для src/
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
 };
