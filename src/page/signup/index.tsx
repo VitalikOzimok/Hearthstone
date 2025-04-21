5efd816edd40ef37d5a6806a59a2f8e4879a7dcc
VitalikOzimok <pilteev111@gmail.com>
2025-04-21 09:30:32 +0300

    chore: незначительные изменения

--- src/page/signup/index.tsx
+++ src/page/signup/index.tsx
@@ -6,35 +6,33 @@ import { fields } from "./constants";
 export function SignUp() {
   const [error, setError] = useState<string | null>(null);
   return (
-    <>
-      <div className="flex flex-col items-center w-4/6 mx-auto  gap-10">
-        <Header text1={"Приключение начинается!"} text2={"Регистрация"} />
-        <div className="text-red-500 text-xl">{error}</div>
-        <Form
-          setError={setError}
-          fields={fields}
-          buttonText={"Зарегистрироваться"}
-          onSubmit={async (data, dispatch) => {
-            const userData = {
-              user: {
-                login: data.login,
-                email: data.email,
-                password: data.password,
-              },
-              token: Date.now() + "_" + Math.random().toString(36).slice(2),
-              isAuthenticated: true,
-            };
+    <div className="flex flex-col items-center w-4/6 mx-auto  gap-10">
+      <Header text1={"Приключение начинается!"} text2={"Регистрация"} />
+      <div className="text-red-500 text-xl">{error}</div>
+      <Form
+        setError={setError}
+        fields={fields}
+        buttonText={"Зарегистрироваться"}
+        onSubmit={async (data, dispatch) => {
+          const userData = {
+            user: {
+              login: data.login,
+              email: data.email,
+              password: data.password,
+            },
+            token: Date.now() + "_" + Math.random().toString(36).slice(2),
+            isAuthenticated: true,
+          };
 
-            localStorage.setItem("auth", JSON.stringify(userData));
-            dispatch({
-              type: "LOGIN",
-              payload: userData,
-            });
+          localStorage.setItem("auth", JSON.stringify(userData));
+          dispatch({
+            type: "LOGIN",
+            payload: userData,
+          });
 
-            return true;
-          }}
-        />
-      </div>
-    </>
+          return true;
+        }}
+      />
+    </div>
   );
 }
